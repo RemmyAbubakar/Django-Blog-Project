3b2325a363e6309b1ce0d740e9e9f10ceb3644bd
Remmy Abubakar <remmybello123@gmail.com>
2024-07-20 18:29:56 +0000

    debug-settings

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 set -e  # Exit immediately if a command exits with a non-zero status.
+trap 'echo "An error occurred. Exiting..."; exit 1;' ERR
 
 echo "BUILD START"
 
+# Log Python version
+python3.9 --version
+
 # Install dependencies
+echo "Installing dependencies..."
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
 # Collect static files
+echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
